1bef32793aac5772f257e9505b7b2f5f279785a7
Giorgio27 <giopag27@gmail.com>
2020-09-09 12:05:45 +0200

    promise in start server

--- src/start.js
+++ src/start.js
@@ -2,6 +2,49 @@ import Koa from 'koa'
 import { getRouter } from './routes'
 import logger from 'loglevel'
 
+const setupCloseOnExit = (server) => {
+  const exitHandler = async (options = {}) => {
+    await server
+      .close()
+      .then(() => {
+        logger.info('Server successfully closed')
+      })
+      .catch((e) => {
+        logger.warn('Something went wrong in close', e.stack)
+      })
+
+    if (options.exit) process.exit()
+  }
+  // do something when app is closing
+  process.on('exit', exitHandler)
+
+  // catches ctrl+c event
+  process.on('SIGINT', exitHandler.bind(null, { exit: true }))
+
+  // catches "kill pid" (for example: nodemon restart)
+  process.on('SIGUSR1', exitHandler.bind(null, { exit: true }))
+  process.on('SIGUSR2', exitHandler.bind(null, { exit: true }))
+
+  // catches uncaught exceptions
+  process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
+}
+
+const promisedServer = (app, port) => {
+  return new Promise((resolve) => {
+    const server = app.listen(port, () => {
+      logger.info(`Listening on port ${server.address().port}`)
+      const originalClose = server.close.bind(server)
+      server.close = () => {
+        return new Promise((resolveClose) => {
+          originalClose(resolveClose)
+        })
+      }
+      setupCloseOnExit(server)
+      resolve(server)
+    })
+  })
+}
+
 export const startServer = ({ port = process.env.PORT } = {}) => {
   const app = new Koa()
 
@@ -37,5 +80,5 @@ export const startServer = ({ port = process.env.PORT } = {}) => {
 
   // port listen
 
-  return app.listen(port)
+  return promisedServer(app, port)
 }
