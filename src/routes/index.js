851ba8e51814b95ea37bb52409a7a0d5ad6a3de7
Giorgio27 <giopag27@gmail.com>
2020-09-09 11:05:08 +0200

    autoreview

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,3 @@
-// import express from 'express'
-// import { getMathRoutes } from './math'
-
-// export const getRoutes = () => {
-//   const router = express.Router()
-//   router.use('/math', getMathRoutes())
-//   return router
-// }
-
 import Router from 'koa-router'
 import mathRouter from './math'
 
