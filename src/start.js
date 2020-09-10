8b5878719666b617d92811e787027a8d37f0da14
Giorgio27 <giopag27@gmail.com>
2020-09-10 11:54:32 +0200

    small improvements in start server

--- src/start.js
+++ src/start.js
@@ -48,15 +48,17 @@ const promisedServer = (app, port) => {
 export const startServer = ({ port = process.env.PORT } = {}) => {
   const app = new Koa()
 
-  // logger
+  app.on('error', (err, ctx) => {
+    logger.info(`${ctx.method} ${ctx.url}`)
+    logger.error('server error:', err)
+    logger.info('ctx:', ctx)
+  })
 
   app.use(async (ctx, next) => {
     await next()
     const rt = ctx.response.get('X-Response-Time')
     logger.info(`${ctx.method} ${ctx.url} - ${rt}`)
   })
-
-  // x-response-time
 
   app.use(async (ctx, next) => {
     const start = Date.now()
@@ -65,20 +67,8 @@ export const startServer = ({ port = process.env.PORT } = {}) => {
     ctx.set('X-Response-Time', `${ms}ms`)
   })
 
-  // error handling
-
-  app.on('error', (err, ctx) => {
-    logger.info(`${ctx.method} ${ctx.url}`)
-    logger.error('server error:', err)
-    logger.info('ctx:', ctx)
-  })
-
-  // router
-
   let router = getRouter()
-  app.use(router.routes()).use(router.allowedMethods())
-
-  // port listen
+  app.use(router.routes())
 
   return promisedServer(app, port)
 }
